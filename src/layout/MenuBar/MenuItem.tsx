7cb74aa2bf2a184ceff835d0f4c0a96616321b81
KOBEsphinx242 <ado.coneo@gmail.com>
2021-07-28 22:02:44 -0500

    feat(menuBar): Add ToolTip for Menu Items

--- src/layout/MenuBar/MenuItem.tsx
+++ src/layout/MenuBar/MenuItem.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import classes from './MenuItem.module.css';
+
 interface MenuItemProps {
   label: string;
   icon: string;
@@ -9,12 +11,12 @@ interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = ({ label, icon, route }) => {
   return (
-    <li>
+    <li className={classes.menuItem}>
       <Link to={route}>
         <i className={icon}></i>
         <span className="links_name">{label}</span>
       </Link>
-      {/*<span className="tooltip">Dashboard</span>*/}
+      <span className={classes.tooltip}>{label}</span>
     </li>
   );
 };
